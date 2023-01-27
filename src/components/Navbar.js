6b17abd866cf0bcd5cc2a40c30c3c7ba0e71037f
adinduokoro <107534645+adinduokoro@users.noreply.github.com>
2023-01-26 21:17:26 -0500

    Working on the search function.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import { signOut } from 'firebase/auth'
-import React from 'react'
+import React, { useContext } from 'react'
+import {signOut} from "firebase/auth"
 import { auth } from '../firebase'
+import { AuthContext } from '../context/AuthContext'
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
-      <span className='logo'>Clubhouse Chat</span>
-      <div className='user'>
-        <img src="https://pbs.twimg.com/profile_images/1564375315526275072/7Xnua7BM_400x400.jpg" alt="" />
-        <span>John</span>
-        <button onClick={() => signOut(auth)}>logout</button>
+      <span className="logo">Test Chat</span>
+      <div className="user">
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={()=>signOut(auth)}>logout</button>
       </div>
     </div>
   )
